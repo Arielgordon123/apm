caefae97081c9a945cd077549abe9c6c4ea7ecb7
ariel <arielgordon123@gmail.com>
2020-02-04 12:11:34 +0200

    lint

--- src/apm/__init__.py
+++ src/apm/__init__.py
@@ -1,31 +1,19 @@
 
 import os
-import errno
 import requests
 import shutil
 import subprocess
-import sys
-
 from zipfile import ZipFile
 from argparse import ArgumentParser
 
 temp_folder = os.path.dirname("./.temp/")
 
+
 def parse_args():
     parser = ArgumentParser()
-    
-    # parser.add_argument("-p", "--publish", dest="filename",
-    #     help="Sets the package you want to publish", metavar="FILE")
-    # parser.add_argument("-r", "--requirements", dest="requirements", metavar="requirements",
-    #     help="Sets the `requirements` list", default="requirements.txt")
-    # parser.add_argument("-t", "--tag", dest="tag",
-        # help="Sets tag `latest` if no --tag specified", default="latest")   
     parser.add_argument("-i", "--install", dest="package",
-        help="the package that you want to install", metavar="package Name", required=True)
-
-    # parser.add_argument("-q", "--quiet",
-    #     action="store_false", dest="verbose", default=True,
-    #     help="don't print status messages to stdout")
+                        help="the package that you want to install",
+                        metavar="package Name", required=True)
 
     args = parser.parse_args()
     get_all_dependencies_recursive(args.package)
@@ -34,8 +22,9 @@ def parse_args():
 
 def get_dependencies(wheel_fname):
     global temp_folder
-    packages = {'inhouse': []}
-    if wheel_fname.startswith("http"): # download if the file from the internet
+    inhouse = []
+    # download if the file from the internet
+    if wheel_fname.startswith("http"):
         create_temp_folder()
         temp_file = os.path.join(temp_folder, wheel_fname.split("/")[-1])
         response = requests.get(wheel_fname, stream=True)
@@ -48,34 +37,34 @@ def get_dependencies(wheel_fname):
         if f.endswith("dependency_links.txt"):
             for l in archive.open(f).read().decode("utf-8").split("\n"):
                 if len(l) > 0:
-                    packages['inhouse'].append(l)
+                    inhouse.append(l)
 
-    return packages
+    return inhouse
+
 
 def create_temp_folder():
     global temp_folder
     if not os.path.exists(temp_folder):
-        try:
-            os.makedirs(temp_folder)
-        except OSError as exc: # Guard against race condition
-            if exc.errno != errno.EEXIST:
-                raise
+        os.makedir(temp_folder)
+
 
 def install_from_local_req_file():
-    subprocess.check_call(["pipenv", "install", "-r", "local_requirements.txt"])
+    subprocess.check_call(
+        ["pipenv", "install", "-r", "local_requirements.txt"])
 
-def get_all_dependencies_recursive(dependencies):  
+
+def get_all_dependencies_recursive(dependencies):
     dependencies = get_dependencies(dependencies)
-    lst = []  
-    with open("local_requirements.txt", "a") as req:
-        for sub_dep in dependencies['inhouse']:
-            req.write(sub_dep+"\r\n")
+    lst = []
+    with open("requirements.inhouse.txt", "a") as requirements:
+        for sub_dep in dependencies:
+            requirements.write(sub_dep+"\r\n")
             get_all_dependencies_recursive(sub_dep)
     return lst
 
+
 def main():
     parse_args()
-    
 
 
 if __name__ == "__main__":
